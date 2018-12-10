d2186d812ea91aac21000de25dd5ce0e33ff12f5
gdhatric <gdhatric@deloitte.com>
2018-12-11 00:28:47 +0700

    First Draft version

---
+++ src/test/java/com/dhatric/domain/JavaRebootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dhatric.domain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaRebootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
